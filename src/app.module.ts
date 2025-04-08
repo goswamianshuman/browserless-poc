ae96addcbf9dab0b5da93bfb315df3b1beba63c2
username <email>
2025-04-08 13:12:10 +0530

    add-items

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'; 
+import { ConfigModule, ConfigService } from '@nestjs/config'; 
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+import configuration from './config/configuration';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({ 
+  imports: [ 
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration], }), 
+    TypeOrmModule.forRootAsync({ 
+      imports: [ConfigModule], 
+      inject: [ConfigService], 
+      useFactory: 
+      (config: ConfigService) => ({ 
+        type: 'postgres', 
+        host: config.get('database.host'), 
+        port: config.get<number>('database.port'), 
+        username: config.get('database.username'), 
+        password: config.get('database.password'), 
+        database: config.get('database.name'), 
+        synchronize: true, 
+        autoLoadEntities: true, 
+      }), 
+    }), 
+  ], 
+}) 
+
 export class AppModule {}
