85380af7e45fd6c7f5c2520fce3303c764c242a7
Anshuman Goswami <anshumangoswami07@gmail.com>
2025-04-09 16:15:44 +0530

    updated console.log

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,13 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(PORT);
-  console.log(`🚀 Application started at: http://localhost:${PORT}`);
+  console.log(`
+    \x1b[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m
+    \x1b[32m🚀🚀🚀       FOOD DELIVERY BACKEND API STARTED       🚀🚀🚀\x1b[0m
+    \x1b[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m
+    🌍  \x1b[1mServer is running at:\x1b[0m \x1b[4mhttp://localhost:${PORT}\x1b[0m
+    💡  Use Ctrl+C to stop the server
+    \x1b[36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\x1b[0m
+    `);
 }
 bootstrap();
